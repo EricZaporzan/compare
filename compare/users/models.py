5ba60639120b1c58656cf1891691536493583db0
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-01-05 00:48:15 -0700

    Added a country field to the user model.

--- compare/users/models.py
+++ compare/users/models.py
@@ -7,14 +7,17 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+from django_countries.fields import CountryField
+
 
 @python_2_unicode_compatible
 class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField(_("Real name"), blank=True, max_length=255)
-    about = models.TextField(_("A little something about yourself"), blank=True)
+    name = models.CharField(_("What's your name?"), blank=True, max_length=255)
+    about = models.TextField(_("A little something about yourself?"), blank=True)
+    country = CountryField(_("Where do you live?"), blank=True, blank_label='(select country)')
 
     def __str__(self):
         return self.username
