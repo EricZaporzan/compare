26ccc8f3505db2a8695521cc23f77a2c2162dc62
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-01-05 23:46:27 -0700

    Added the comparisons model and registered it with the admin. Now we're cooking.

--- compare/comparisons/admin.py
+++ compare/comparisons/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comparison
+
+class ComparisonAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "active",)
+
+admin.site.register(Comparison, ComparisonAdmin)
--- compare/comparisons/models.py
+++ compare/comparisons/models.py
@@ -1,5 +1,21 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
 
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from compare.users.models import User
+
+class Comparison(models.Model):
+    owner = models.ForeignKey(User)
+    title = models.CharField(_("What should it be called?"), max_length=255)
+    description = models.TextField(_("Outline the rules, guidelines, and anything else important about this comparison."), blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_ending = models.DateTimeField(_("When should we finish?"), blank=True)
+    active = models.BooleanField(_("Is it happening right now?"), default=True)
+
+    def __str__(self):
+        return self.title
