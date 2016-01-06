5ba075be37297e7bddb9e0cf2f7096d438dfc95d
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-01-06 00:24:28 -0700

    Register the comparisonitem model with the admin

--- compare/comparisons/admin.py
+++ compare/comparisons/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Comparison
+from .models import Comparison, ComparisonItem
 
 class ComparisonAdmin(admin.ModelAdmin):
     list_display = ("title", "owner", "active",)
 
+class ComparisonItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner",)
+
 admin.site.register(Comparison, ComparisonAdmin)
+admin.site.register(ComparisonItem, ComparisonItemAdmin)
