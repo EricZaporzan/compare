823dba8eb55258b8c2479fafa96ca45a56b11a59
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-01-07 03:27:51 -0700

    Added comparison create and detail views and the templates for same. Totally rudimentary right now, but progress!

--- compare/comparisons/urls.py
+++ compare/comparisons/urls.py
@@ -7,5 +7,25 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='comparisons/home.html'), name="home")
+    url(r'^$', TemplateView.as_view(template_name='comparisons/home.html'), name="home"),
+    url(
+        regex=r'^$',
+        view=views.ComparisonListView.as_view(),
+        name='list'
+    ),
+    url(
+        regex=r'^~create/$',
+        view=views.ComparisonCreateView.as_view(),
+        name='create'
+    ),
+    url(
+        regex=r'^(?P<pk>[\w.@+-]+)/$',
+        view=views.ComparisonDetailView.as_view(),
+        name='detail'
+    ),
+    url(
+        regex=r'^~update/$',
+        view=views.ComparisonUpdateView.as_view(),
+        name='update'
+    ),
 ]
--- compare/comparisons/views.py
+++ compare/comparisons/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-# Create your views here.
-def index(request):
-    return render(request, "comparisons/home.html", {})
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView, DetailView, ListView, RedirectView, UpdateView
+
+from braces.views import LoginRequiredMixin
+
+from .models import Comparison, ComparisonItem
+
+class ComparisonCreateView(LoginRequiredMixin, CreateView):
+    fields = ['title', 'owner', 'description', 'date_starting', 'date_ending',]
+    model = Comparison
+
+
+class ComparisonUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comparison
+
+class ComparisonDetailView(LoginRequiredMixin, DetailView):
+    model = Comparison
+    slug_field = "pk"
+    slug_url_kwarg = "pk"
+
+class ComparisonListView(ListView):
+    model = Comparison
