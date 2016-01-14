80e04ce9ac32174eece26d144bec6a1db69a79a8
EricZaporzan <eric.zaporzan@gmail.com>
2016-01-14 06:10:20 -0500

    Added some form labels. Crispy forms are kind of amazing.

--- compare/comparisons/forms.py
+++ compare/comparisons/forms.py
@@ -4,10 +4,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Fieldset, ButtonHolder, Submit
 
 class ComparisonCreateForm(forms.Form):
-    title = forms.CharField(required = True)
-    description = forms.CharField(required = False, widget=forms.Textarea)
-    date_starting = forms.DateField(required = False)
-    date_ending = forms.DateField(required = False)
+    title = forms.CharField(required=True, label="What should it be called?")
+    description = forms.CharField(required=False, widget=forms.Textarea, label="Outline the rules, guidelines, and anything else important about this comparison.")
+    date_starting = forms.DateField(required=False, label="When should we start?")
+    date_ending = forms.DateField(required=False, label="When should we finish?")
 
     def __init__(self, *args, **kwargs):
         super(ComparisonCreateForm, self).__init__(*args, **kwargs)
