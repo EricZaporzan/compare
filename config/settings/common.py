2f8fa3c0dd420f24b84c215bdbbc8c1fee5b4f22
EricZaporzan <eric.zaporzan@gmail.com>
2016-01-16 17:02:20 -0500

    Adding facebook authentication. I want to hold off on integrating this.

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.facebook',
     'django_countries', # Country field for the user model
     'sorl.thumbnail', # Utility for quickly creating thumbnails
 )
