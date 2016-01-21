f0c61e7140c26bcc71d6eb927df408805d684790
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-01-21 12:39:56 -0500

    We will need a serializer for ComparisonItems as well. The idea will be to GET two(+?) comparisonitems, and then POST the vote once the user chooses their favourite.

--- compare/comparisons/serializers.py
+++ compare/comparisons/serializers.py
@@ -3,5 +3,8 @@ from rest_framework import serializers
 from .models import ComparisonItemVote
 
 
+class ComparisonItemSerializer(serializers.Serializer):
+    date_created = serializers.DateTimeField(read_only=True)
+
 class ComparisonItemVoteSerializer(serializers.Serializer):
     date_created = serializers.DateTimeField(read_only=True)
